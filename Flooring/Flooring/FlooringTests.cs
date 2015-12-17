84c7e532b7588805d401dd953f66805d7bf93a08
norisradu <noris.gavrilas@yahoo.com>
2015-12-17 23:49:54 +0200

    Nu-mi dau seama unde e eroarea

--- Flooring/Flooring/FlooringTests.cs
+++ Flooring/Flooring/FlooringTests.cs
@@ -4,11 +4,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Flooring
 {
     [TestClass]
-    public class UnitTest1
+    public class FlooringTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MostSimpleExample()
         {
+            float neededPieces = CalculateFloorPieces(1, 1, 1, 1);
+            Assert.AreEqual(2, neededPieces);
+        }
+        float CalculateFloorPieces(int roomLength, int roomWidth, int pieceLength, int pieceWidth)
+        {
+            int roomArea = roomLength * roomWidth;
+            int pieceArea = pieceLength * pieceWidth;
+            int principalNumberOfNeededPieces = roomArea / pieceArea;
+            float loss = (15 / 100) * principalNumberOfNeededPieces;
+            float finalNumberOfNeededPieces = principalNumberOfNeededPieces + loss;
+            Math.Ceiling(finalNumberOfNeededPieces);
+            return (finalNumberOfNeededPieces);
         }
     }
 }
