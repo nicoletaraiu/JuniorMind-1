9be5b5e42599e66dea8e84c2e0b842bc00dcc6bb
norisradu <noris.gavrilas@yahoo.com>
2015-12-17 16:00:09 +0200

    Calculate bird's distance for a greater decimal distance between trains

--- BirdDistance/BirdDistance/BirdDistanceTests.cs
+++ BirdDistance/BirdDistance/BirdDistanceTests.cs
@@ -12,6 +12,11 @@ namespace BirdDistance
             decimal birdDistance = CalculateBirdDistance(200, 100);
             Assert.AreEqual(50, birdDistance);
         }
+        public void BirdDistanceForAGreaterDecimalDistance()
+        {
+            decimal birdDistance = CalculateBirdDistance(350, 1345.78m);
+            Assert.AreEqual(672.89m, birdDistance);
+        }
         decimal CalculateBirdDistance(decimal trainSpeed, decimal distanceBetweenTrains)
         {
             return distanceBetweenTrains / 2;
