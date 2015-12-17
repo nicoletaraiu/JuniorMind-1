f0e384b6e3e8ae687b1f2644b74e7e1336a3ad85
norisradu <noris.gavrilas@yahoo.com>
2015-12-17 15:22:08 +0200

    Calculate repetitions number for a pyramid top of 4

---
+++ Sportsman/Sportsman/SportsmanTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sportsman
+{
+    [TestClass]
+    public class SportsmanTests
+    {
+        [TestMethod]
+        public void SixteenRepetitions()
+        {
+            int repetitionsNumber = CalculateRepetitionsNumber(4);
+            Assert.AreEqual(16, repetitionsNumber);
+        }
+        int CalculateRepetitionsNumber(int top)
+        {
+            return top * top;
+        }
+    }
+}
