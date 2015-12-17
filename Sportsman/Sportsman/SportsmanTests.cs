711c6bc7f4dd688c5fc79a3ba57d25d60e183352
norisradu <noris.gavrilas@yahoo.com>
2015-12-17 15:25:01 +0200

    Calculate repetitions number for a pyramid top of 6

--- Sportsman/Sportsman/SportsmanTests.cs
+++ Sportsman/Sportsman/SportsmanTests.cs
@@ -12,6 +12,12 @@ namespace Sportsman
             int repetitionsNumber = CalculateRepetitionsNumber(4);
             Assert.AreEqual(16, repetitionsNumber);
         }
+        [TestMethod]
+        public void ThirtysixRepetitions()
+        {
+            int repetitionsNumber = CalculateRepetitionsNumber(6);
+            Assert.AreEqual(36, repetitionsNumber);
+        }
         int CalculateRepetitionsNumber(int top)
         {
             return top * top;
