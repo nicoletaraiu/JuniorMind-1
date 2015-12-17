d9d5dfca0592c57af46e3a48938b76d707a8189e
norisradu <noris.gavrilas@yahoo.com>
2015-12-17 21:50:38 +0200

    Calculate stone quantity for a 4X4 square & 2X2 stones

--- PublicSquare/PublicSquare/PublicSquareTests.cs
+++ PublicSquare/PublicSquare/PublicSquareTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PublicSquare
 {
     [TestClass]
-    public class UnitTest1
+    public class PublicSquareTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PerfectSquareTest()
         {
+            float cubicalStone = CalculateCubicalStoneQuantity(4, 4, 2, 2);
+            Assert.AreEqual(4, cubicalStone);
+        }
+        float CalculateCubicalStoneQuantity(int publicSquareLength, int publicSquareWidth, int stoneLength, int stoneWidth)
+        {
+            return (publicSquareLength / stoneLength) * (publicSquareWidth / stoneWidth);
         }
     }
 }
