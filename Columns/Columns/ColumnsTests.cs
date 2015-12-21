2c75e1cab307613ea58c786d901da3731ab87392
norisradu <noris.gavrilas@yahoo.com>
2015-12-21 22:29:09 +0200

    Calculate area for a simple, but impossible scenario

---
+++ Columns/Columns/ColumnsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Columns
+{ 
+    [TestClass]
+    public class ColumnsTests
+    {
+        [TestMethod]
+        public void FirstArea()
+        {
+        float area = CalculateArea(1, 1, 1, 1, 1, 1);
+        Assert.AreEqual(0, area);
+        }
+         float CalculateArea(float firstX, float firstY, float secondX, float secondY, float thirdX, float thirdY)
+        {
+            return 0;
+        }
+    }
+}
