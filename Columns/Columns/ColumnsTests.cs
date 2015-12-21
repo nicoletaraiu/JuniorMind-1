90772769803a7b32c257199bf410f16383676922
norisradu <noris.gavrilas@yahoo.com>
2015-12-21 22:55:19 +0200

    Calculate area for a negative determinant resulted

--- Columns/Columns/ColumnsTests.cs
+++ Columns/Columns/ColumnsTests.cs
@@ -18,10 +18,17 @@ namespace Columns
             float area = CalculateArea(2, 2, 3, 3, 1, 2);
             Assert.AreEqual(0.5, area);
         }
-         float CalculateArea(float Xa, float Ya, float Xb, float Yb, float Xc, float Yc)
+        [TestMethod]
+        public void NegativeDeterminantTry()
         {
+            float area = CalculateArea(7, 6, 3, 4, 2, 5);
+            Assert.AreEqual(3, area);
+        }
+         float CalculateArea(float Xa, float Ya, float Xb, float Yb, float Xc, float Yc)
+        { 
             float determinant = Xa * Yb + Xb * Yc + Ya * Xc - Yb * Xc - Yc * Xa - Ya * Xb;
-            float area = 0.5f * determinant;
+            float positiveDeterminant = Math.Abs(determinant);
+            float area = 0.5f * positiveDeterminant;
             return area;
         } 
     }
