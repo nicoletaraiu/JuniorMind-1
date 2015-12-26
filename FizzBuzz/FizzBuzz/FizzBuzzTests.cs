0afe4f6d508afdc993ba56c1807003c624a4606e
norisradu <noris.gavrilas@yahoo.com>
2015-12-26 11:35:52 +0200

    Problem finished

--- FizzBuzz/FizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzTests.cs
@@ -7,18 +7,28 @@ namespace FizzBuzz
     public class FizzBuzzTests
     {
         [TestMethod]
+        public void Fizz()
+        {
+            Assert.AreEqual("Fizz", VerifyFizzBuzz(3));
+        }
+        [TestMethod]
+        public void Buzz()
+        {
+            Assert.AreEqual("Buzz", VerifyFizzBuzz(5));
+        }
+        [TestMethod]
         public void FizzBuzz()
         {
-            Assert.AreEqual("Fizz", VerifyFizz(3));
+            Assert.AreEqual("FizzBuzz", VerifyFizzBuzz(15));
         }
-        string VerifyFizz(int n)
+        string VerifyFizzBuzz(int n)
         {
             string output = "";
             if (n % 3 == 0) output = "Fizz";
+            if (n % 5 == 0) output = "Buzz";
+            if ((n % 3 == 0) && (n % 5 == 0)) output = "FizzBuzz";
             return output;
-            ///else if (n % 5 == 0) output = "Buzz";
-            
-        }
+        } 
         }
     }
 
