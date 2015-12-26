8d51d1e358124295a27f97b84221b2db33e358c7
norisradu <noris.gavrilas@yahoo.com>
2015-12-26 12:56:13 +0200

    Calculate initial area for a 6mp resulted field, 2mp new width

--- Field/Field/FieldTests.cs
+++ Field/Field/FieldTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Field
 {
     [TestClass]
-    public class UnitTest1
+    public class FieldTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InitialAreaForASimpleScenario()
         {
+            float initialArea = CalculateInitialArea(2, 6);
+            Assert.AreEqual(4, initialArea);
+        }
+        float CalculateInitialArea (int newWidth, int finalArea)
+        {
+            return 0;
         }
     }
 }
