e8ba647b735525bc12294dac01e8039b4d5cac50
norisradu <noris.gavrilas@yahoo.com>
2016-01-04 14:39:49 +0200

    Calculate area for a first simple scenario

--- Field/Field/FieldTests.cs
+++ Field/Field/FieldTests.cs
@@ -9,12 +9,21 @@ namespace Field
         [TestMethod]
         public void InitialAreaForASimpleScenario()
         {
-            float initialArea = CalculateInitialArea(2, 6);
-            Assert.AreEqual(4, initialArea);
+            float initialArea = CalculateInitialArea(1, 6);
+            Assert.AreEqual(2, initialArea);
         }
         float CalculateInitialArea (int newWidth, int finalArea)
         {
-            return 0;
+            
+            double solution1 = 0;
+            /// finalArea = initialWidth * initialWidth + newWidth * initialWidth;
+            int discriminant = newWidth * newWidth + 4 * finalArea;
+            if (discriminant > 0)
+            {
+                solution1 = (- newWidth + Math.Sqrt(discriminant)) / 2;
+                return (float)solution1;
+            }
+            else return 0;
         }
     }
 }
