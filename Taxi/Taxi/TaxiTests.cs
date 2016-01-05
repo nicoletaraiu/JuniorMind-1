4a6d54b6b7a20ae0bf917e1a5711e0138cee5b0d
norisradu <noris.gavrilas@yahoo.com>
2016-01-05 11:00:17 +0200

    Tests for day and night hours

--- Taxi/Taxi/TaxiTests.cs
+++ Taxi/Taxi/TaxiTests.cs
@@ -15,20 +15,42 @@ namespace Taxi
         [TestMethod]
         public void SecondSimpleTest()
         {
-            int cost = CalculateCost(8, 35);
-            Assert.AreEqual(140, cost);
+            int cost = CalculateCost(10, 35);
+            Assert.AreEqual(280, cost);
+        }
+        [TestMethod]
+        public void FirstNightTest()
+        {
+            int cost = CalculateCost(7, 10);
+            Assert.AreEqual(70, cost);
+        }
+        [TestMethod]
+        public void SecondNightTest()
+        {
+            int cost = CalculateCost(23, 41);
+            Assert.AreEqual(410, cost);
+        }
+        [TestMethod]
+        public void ThirdNightTest()
+        {
+            int cost = CalculateCost(1, 80);
+            Assert.AreEqual(640, cost);
         }
         int CalculateCost(int daytime, int distance)
         {
             int cost = 0;
-           
+            if ((daytime >= 8) && (daytime <= 21))
             {
                 if ((distance >= 1) && (distance <= 20)) cost = 5;
-                else if ((distance >= 21) && (distance <= 60)) cost = 4;
-                else if ((distance >= 60)) cost = 3;
-                return cost * distance;
-            }
-           
+                else if ((distance >= 21) && (distance <= 60)) cost = 8;
+                else if ((distance >= 60)) cost = 6;
+            } 
+            else {
+                if ((distance >= 1) && (distance <= 20)) cost = 7;
+                else if ((distance >= 21) && (distance <= 60)) cost = 10;
+                else if ((distance >= 60)) cost = 8;
+             }
+            return cost * distance;
         }
     }
 }
