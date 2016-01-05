9402b1cc5a841e3951cc986dad6154db626f02d3
norisradu <noris.gavrilas@yahoo.com>
2016-01-05 10:15:50 +0200

    Calculate total cost if we are one day late

--- RentalCosts/RentalCosts/RentalCostsTests.cs
+++ RentalCosts/RentalCosts/RentalCostsTests.cs
@@ -4,11 +4,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace RentalCosts
 {
     [TestClass]
-    public class UnitTest1
+    public class RentalCostsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void OneDayLate()
         {
+            float totalCost = CalculatePenalty(100, 1);
+            Assert.AreEqual(102, totalCost);
+        }
+        float CalculatePenalty(float monthlyCost, int additionalDays)
+        {
+            float penalty = 0;
+            if ((additionalDays >= 1) && (additionalDays <= 10))
+                penalty = (float)0.02 * monthlyCost;
+            else if ((additionalDays >= 11) && (additionalDays <= 30))
+                penalty = (float)0.05 * monthlyCost;
+            else if ((additionalDays >= 31) && (additionalDays <= 40))
+                penalty = (float)0.1 * monthlyCost;
+            return (float)(monthlyCost + penalty);
         }
     }
 }
