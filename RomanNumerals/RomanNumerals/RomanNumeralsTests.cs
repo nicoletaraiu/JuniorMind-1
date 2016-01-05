ae8d0c288821ac4522d40f5f40f600edf54572bb
norisradu <noris.gavrilas@yahoo.com>
2016-01-05 11:59:03 +0200

    First numeral test

---
+++ RomanNumerals/RomanNumerals/RomanNumeralsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumerals
+{
+    [TestClass]
+    public class RomanNumeralsTests
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            string roman = ConvertArabicToRoman(1);
+            Assert.AreEqual("I", roman);
+        }
+        string ConvertArabicToRoman(int arabic)
+        {
+            string output = "";
+            if (arabic == 1) output = "I";
+            return output;
+        }
+    }
+}
