8b0c2a094398c3bbe08b3a214aeba5934a1653f5
norisradu <noris.gavrilas@yahoo.com>
2016-01-05 10:49:10 +0200

    Simple cases

---
+++ Taxi/Taxi/TaxiTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taxi
+{
+    [TestClass]
+    public class TaxiTests
+    {
+        [TestMethod]
+        public void FirstSimpleTest()
+        {
+            int cost = CalculateCost(8, 15);
+            Assert.AreEqual(75, cost);
+        }
+        [TestMethod]
+        public void SecondSimpleTest()
+        {
+            int cost = CalculateCost(8, 35);
+            Assert.AreEqual(140, cost);
+        }
+        int CalculateCost(int daytime, int distance)
+        {
+            int cost = 0;
+           
+            {
+                if ((distance >= 1) && (distance <= 20)) cost = 5;
+                else if ((distance >= 21) && (distance <= 60)) cost = 4;
+                else if ((distance >= 60)) cost = 3;
+                return cost * distance;
+            }
+           
+        }
+    }
+}
