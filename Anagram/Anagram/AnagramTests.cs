0f918ff79618e928773c8512e8c2aa3609eeff9f
norisradu <noris.gavrilas@yahoo.com>
2016-01-07 21:44:00 +0200

    My function

--- Anagram/Anagram/AnagramTests.cs
+++ Anagram/Anagram/AnagramTests.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Anagram
 {
     [TestClass]
-    public class UnitTest1
+    public class AnagramTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+        }
+        int CalculateAnagramsNumber(string word)
+        {
+            int anagramsNumber = 0;
+            int lettersNumber = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                lettersNumber++;
+            }
+            anagramsNumber = 
         }
     }
 }
