6d56a34fe70a8d9d40d650aa2f3078240361367b
norisradu <noris.gavrilas@yahoo.com>
2016-01-09 17:23:55 +0200

    Anagram final

--- Anagram/Anagram/AnagramTests.cs
+++ Anagram/Anagram/AnagramTests.cs
@@ -21,15 +21,45 @@ namespace Anagram
         {
             Assert.AreEqual(120, (CalculateAnagramsNumber("abcde")));
         }
+        [TestMethod]
+        public void TrickyWord()
+        {
+            Assert.AreEqual(3, (CalculateAnagramsNumber("aab")));
+        }
+        [TestMethod]
+        public void SecondTrickyWord()
+        {
+            Assert.AreEqual(20, (CalculateAnagramsNumber("aabac")));
+        }
         int CalculateAnagramsNumber(string word)
         {
-            int anagramsNumber = word.Length;
-            for (int i = 1; i < word.Length; i++)
+            int allOccurrences = 1;
+            for (int i = 'a'; i <= 'z'; i++)
             {
-                anagramsNumber = anagramsNumber * i;
+                allOccurrences *= Factorial(CalculateOccurrencesNumber((char)i, word));
             }
-            return anagramsNumber;
+            return (Factorial(word.Length)) / allOccurrences;
         }
-        
+        int CalculateOccurrencesNumber(char letter, string word)
+        {
+            int occurrencesNumber = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (letter == word[i])
+                    occurrencesNumber++;
+            }
+            return occurrencesNumber;
+        }
+        int Factorial (int number)
+        {
+            int result = 1;
+            while (number > 0)
+            {
+                result *= number;
+                number--;
+            }
+            return result;
+        }
+        }
     }
-}
+
