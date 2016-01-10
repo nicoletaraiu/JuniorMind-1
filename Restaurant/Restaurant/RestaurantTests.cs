c291900678d547d1a49fde4295b07e4e691d51f1
norisradu <noris.gavrilas@yahoo.com>
2016-01-10 14:28:38 +0200

    Test for everyday meetings

--- Restaurant/Restaurant/RestaurantTests.cs
+++ Restaurant/Restaurant/RestaurantTests.cs
@@ -9,28 +9,35 @@ namespace Restaurant
         [TestMethod]
         public void TestForGivenCase()
         {
-            Assert.AreEqual(12, LeastCommonMultiple(4, 6));
+            Assert.AreEqual(13, FindMeetingDay(4, 6));
         }
-        int LeastCommonMultiple(int whenMyFriendGoes, int whenIGo)
+        [TestMethod]
+        public void TestForRichPeople()
+        {
+            Assert.AreEqual(2, FindMeetingDay(1, 1));
+        }
+        int FindMeetingDay(int whenMyFriendGoes, int whenIGo)
         {
             int auxiliar1, auxiliar2;
             if (whenMyFriendGoes > whenIGo)
             {
-                auxiliar1 = whenMyFriendGoes; auxiliar2 = whenIGo;
+                auxiliar1 = whenMyFriendGoes;
+                auxiliar2 = whenIGo;
             }
             else
             {
-                auxiliar1 = whenIGo; auxiliar2 = whenMyFriendGoes;
+                auxiliar1 = whenIGo;
+                auxiliar2 = whenMyFriendGoes;
             }
 
             for (int i = 1; i <= auxiliar2; i++)
             {
                 if ((auxiliar1 * i) % auxiliar2 == 0)
                 {
-                    return i * auxiliar1;
+                    return i * auxiliar1 + 1;
                 }
             }
-            return auxiliar2;
+            return auxiliar2 + 1;
         }
     }
 }
