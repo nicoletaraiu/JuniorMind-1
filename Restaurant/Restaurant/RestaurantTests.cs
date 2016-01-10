65ca2d63fce113ed0f557e080bf37d5d4a333bf1
norisradu <noris.gavrilas@yahoo.com>
2016-01-10 14:22:07 +0200

    Least common multiple

--- Restaurant/Restaurant/RestaurantTests.cs
+++ Restaurant/Restaurant/RestaurantTests.cs
@@ -4,11 +4,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Restaurant
 {
     [TestClass]
-    public class UnitTest1
+    public class RestaurantTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForGivenCase()
         {
+            Assert.AreEqual(12, LeastCommonMultiple(4, 6));
+        }
+        int LeastCommonMultiple(int whenMyFriendGoes, int whenIGo)
+        {
+            int auxiliar1, auxiliar2;
+            if (whenMyFriendGoes > whenIGo)
+            {
+                auxiliar1 = whenMyFriendGoes; auxiliar2 = whenIGo;
+            }
+            else
+            {
+                auxiliar1 = whenIGo; auxiliar2 = whenMyFriendGoes;
+            }
+
+            for (int i = 1; i <= auxiliar2; i++)
+            {
+                if ((auxiliar1 * i) % auxiliar2 == 0)
+                {
+                    return i * auxiliar1;
+                }
+            }
+            return auxiliar2;
         }
     }
 }
