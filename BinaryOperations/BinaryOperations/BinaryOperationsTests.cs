cd0d31bff1ff132f0d57eeeddb5b005811a96403
norisradu <noris.gavrilas@yahoo.com>
2016-01-19 22:17:12 +0200

    Failed test for the transformation function

--- BinaryOperations/BinaryOperations/BinaryOperationsTests.cs
+++ BinaryOperations/BinaryOperations/BinaryOperationsTests.cs
@@ -7,11 +7,27 @@ namespace BinaryOperations
     public class BinaryOperationsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalculateArrayLengthTest()
         {
             Assert.AreEqual(6, (CalculateNeededArrayLength(49)));
         }
-      
+        [TestMethod]
+        public void TransformIntoByteArrayTest()
+        {
+            Assert.AreEqual(110001, (TransformIntoByteArray(49)));
+        }
+         byte[] TransformIntoByteArray(byte number)
+       {
+           byte[] binaryVersion = new byte[CalculateNeededArrayLength(number)];
+           while (number > 0)
+           {
+               binaryVersion[CalculateNeededArrayLength(number) - 1] = (byte)(number % 2);
+               number = (byte)(number / 2);
+           }
+           return binaryVersion;
+
+       } 
+
         int CalculateNeededArrayLength(int number)
         {
             int length = 0;
