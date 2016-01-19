b46046ccc42072e8cdc3e048d4372a9f1e4327a9
norisradu <noris.gavrilas@yahoo.com>
2016-01-19 22:05:51 +0200

    Calculate needed array length

--- BinaryOperations/BinaryOperations/BinaryOperationsTests.cs
+++ BinaryOperations/BinaryOperations/BinaryOperationsTests.cs
@@ -4,11 +4,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BinaryOperations
 {
     [TestClass]
-    public class UnitTest1
+    public class BinaryOperationsTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(6, (CalculateNeededArrayLength(49)));
+        }
+      
+        int CalculateNeededArrayLength(int number)
+        {
+            int length = 0;
+            while (number > 0)
+            {
+                number /= 2;
+                length += 1;
+            }
+            return length;
         }
     }
 }
