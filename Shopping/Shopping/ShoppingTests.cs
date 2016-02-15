ba021eed5a4272d5014744b031229ee93b1e143b
norisradu <noris.gavrilas@yahoo.com>
2016-02-15 22:18:21 +0200

    Total price test passed

---
+++ Shopping/Shopping/ShoppingTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shopping
+{
+    [TestClass]
+    public class ShoppingTests
+    {
+        [TestMethod]
+        public void ShouldCalculateTheTotalPrice()
+        {
+            var prices = new Cart[] { new Cart("milk", 2), new Cart("bread", 1) };
+            Assert.AreEqual(3, CalculateTheTotalPrice(prices));
+        }
+        public struct Cart
+        {
+            public string name;
+            public int price;
+
+            public Cart(string name, int price)
+            {
+                this.name = name;
+                this.price = price;
+            }
+        }
+        static int CalculateTheTotalPrice(Cart[] goods)
+        {
+            int total = 0;
+            for (int i = 0; i < goods.Length; i++)
+                total += goods[i].price;
+            return total;
+        }
+    }
+}
