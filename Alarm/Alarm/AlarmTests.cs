20170af2babb0dc23e95e57c4263023baa06ae40
norisradu <noris.gavrilas@yahoo.com>
2016-02-21 22:44:54 +0200

    Weekend tests passed

--- Alarm/Alarm/AlarmTests.cs
+++ Alarm/Alarm/AlarmTests.cs
@@ -9,7 +9,16 @@ namespace Alarm
         [TestMethod]
         public void SimpleCheck()
         {
-            Assert.AreEqual(1, CheckAlarmStatus(new Alarm(Days.Monday, 8, 0)));
+            Assert.AreEqual(1, CheckAlarmStatus(new Alarm(Days.Monday, 6, 0)));
+            Assert.AreEqual(0, CheckAlarmStatus(new Alarm(Days.Saturday, 6, 0)));
+            Assert.AreEqual(0, CheckAlarmStatus(new Alarm(Days.Tuesday, 8, 0)));
+        }
+        [TestMethod]
+        public void WeekendCheck()
+        {
+            Assert.AreEqual(1, CheckAlarmStatus(new Alarm(Days.Saturday, 8, 0)));
+            Assert.AreEqual(0, CheckAlarmStatus(new Alarm(Days.Sunday, 7, 0)));
+            Assert.AreEqual(0, CheckAlarmStatus(new Alarm(Days.Sunday, 8, 45)));
         }
 
         struct Alarm
@@ -40,7 +49,11 @@ namespace Alarm
         static int CheckAlarmStatus(Alarm alarms)
         {
             int status = 0;
-            if (alarms.day != 0) status = 1;
+            if
+                (((alarms.day == Days.Monday) || (alarms.day == Days.Tuesday) || (alarms.day == Days.Wednesday) || (alarms.day == Days.Thursday) || (alarms.day == Days.Friday)) && (alarms.hour == 6) && (alarms.minute == 0))
+                    status = 1;
+            if (((alarms.day == Days.Saturday) || (alarms.day == Days.Sunday)) && (alarms.hour == 8) && (alarms.minute == 0))
+                status = 1;
 
             return status;
         }
