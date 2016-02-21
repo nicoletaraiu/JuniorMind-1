ef539836f625c837d6c2920aeb06423056ef0a3a
norisradu <noris.gavrilas@yahoo.com>
2016-02-21 21:45:13 +0200

    Building the function

--- Alarm/Alarm/AlarmTests.cs
+++ Alarm/Alarm/AlarmTests.cs
@@ -4,11 +4,41 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Alarm
 {
     [TestClass]
-    public class UnitTest1
+    public class AlarmTests
     {
         [TestMethod]
         public void TestMethod1()
         {
         }
+
+        struct Alarm
+        {
+            public Day day;
+            public int hour;
+            public int minute;
+
+            public Alarm(Day day, int hour, int minute)
+            {
+                this.day = day;
+                this.hour = hour;
+                this.minute = minute;
+            }
+        }
+
+        enum Day
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        /*static bool CheckAlarmStatus(Alarm alarm)
+        {
+            
+        }*/
     }
 }
