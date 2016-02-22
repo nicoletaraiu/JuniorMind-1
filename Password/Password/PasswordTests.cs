c96702ad3d7e93ba91830d03df10af8019936b82
norisradu <noris.gavrilas@yahoo.com>
2016-02-22 23:06:49 +0200

    Constructing the function

--- Password/Password/PasswordTests.cs
+++ Password/Password/PasswordTests.cs
@@ -10,5 +10,36 @@ namespace Password
         public void TestMethod1()
         {
         }
+
+        public struct Password
+        {
+            public string lowercaseLetters;
+            public string uppercaseLetters;
+            public string digits;
+
+            public Password(string lowercaseLetters, string uppercaseLetters, string digits)
+            {
+                this.lowercaseLetters = lowercaseLetters;
+                this.uppercaseLetters = uppercaseLetters;
+                this.digits = digits;
+            }
+        }
+
+        static Random random = new Random();
+        public static char ReturnRandomLowercaseLetter()
+        {   
+            int number = random.Next(0, 26);
+            char letter = (char)('a' + number);
+            return letter;
+        }
+
+        static Password ReturnNeededPassword(int number)
+        {
+            string[] neededPassword = new string[10]; ;
+            for (int i = 0; i < neededPassword.Length; i++)
+            {
+                neededPassword[i].lowercaseLetters = ReturnRandomLowercaseLetter;
+            }
+        }
     }
 }
