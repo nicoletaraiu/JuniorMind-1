626d1ebbd3f842861a75f05ec59f617fb20945c0
norisradu <noris.gavrilas@yahoo.com>
2016-02-23 21:40:43 +0200

    Trying the new function

--- Alarm/Alarm/AlarmTests.cs
+++ Alarm/Alarm/AlarmTests.cs
@@ -8,7 +8,7 @@ namespace Alarm
     {
         [TestMethod]
         public void FirstCheck()
-        {
+        {   
             Assert.AreEqual(true, CheckAlarmStatus(Days.Wednesday, 6, 0));
         }
         [TestMethod]
@@ -25,6 +25,12 @@ namespace Alarm
         public void FourthCheck()
         {
             Assert.AreEqual(false, CheckAlarmStatus(Days.Saturday, 6, 0));
+        }
+        [TestMethod]
+        public void FifthCheck()
+        {
+            Assert.AreEqual(false, NewFunction(Days.Tuesday, Days.Wednesday, 6, 0));
+            Assert.AreEqual(true, NewFunction(Days.Tuesday, Days.Tuesday, 6, 0));
         }
 
         struct Alarm
@@ -54,12 +60,19 @@ namespace Alarm
 
         static bool CheckAlarmStatus(Days day, int hour, int minute)
         {
-            var workingDays = Days.Monday | Days.Tuesday | Days.Wednesday | Days.Thursday | Days.Friday;
+            var workingDays = Days.Monday| Days.Tuesday | Days.Wednesday | Days.Thursday | Days.Friday;
             var weekendDays = Days.Saturday | Days.Sunday;
-
             if (((workingDays & day) == day) && (hour == 6) && (minute == 0))
                 return true;
             return (((weekendDays & day) == day) && (hour == 8) && (minute == 0));
         }
+
+        static bool NewFunction(Days dayWithAlarm, Days day, int hour, int minute)
+        {
+            if (((dayWithAlarm & day) == day) && (hour == 6) && (minute == 0))
+                return true;
+            return (((dayWithAlarm & day) == day) && (hour == 8) && (minute == 0));
+        }
+
     }
 }
