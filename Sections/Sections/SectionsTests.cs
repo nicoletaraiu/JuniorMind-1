3b73b9fb3a78ee4304c71923e88b71c298fd0078
norisradu <noris.gavrilas@yahoo.com>
2016-02-29 22:32:16 +0200

    string replaced by int

--- Sections/Sections/SectionsTests.cs
+++ Sections/Sections/SectionsTests.cs
@@ -4,11 +4,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Sections
 {
     [TestClass]
-    public class UnitTest1
+    public class SectionsTests
     {
         [TestMethod]
         public void TestMethod1()
         {
         }
+
+        public struct Intersection
+        {
+            public int up;
+            public int down;
+            public int right;
+            public int left;
+
+            public Intersection(int up, int down, int right, int left)
+            {
+                this.up = up;
+                this.down = down;
+                this.right = right;
+                this.left = left;
+            }
+        }
+
     }
 }
