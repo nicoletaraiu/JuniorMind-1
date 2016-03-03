f0f9f3b1324f2a131779f323b647883182e1d04c
norisradu <noris.gavrilas@yahoo.com>
2016-03-03 20:32:33 +0200

    Modified function and tests

--- Sections/Sections/SectionsTests.cs
+++ Sections/Sections/SectionsTests.cs
@@ -9,9 +9,16 @@ namespace Sections
         [TestMethod]
         public void FindTheEndOfTheSection()
         {
-            Assert.AreEqual(new Point(0, 1), FindTheEndOfTheSection(new Point(0, 0), new Instructions(true, false, false, false)));
-            Assert.AreEqual(new Point(2, 1), FindTheEndOfTheSection(new Point(3, 2), new Instructions(false, true, false, true)));
-            Assert.AreEqual(new Point(1, 4), FindTheEndOfTheSection(new Point(2, 3), new Instructions(true, false, false, true)));
+            Assert.AreEqual(new Point(0, 1), FindTheEndOfTheSection(new Point(0, 0), new Instructions[] { new Instructions(true, false, false, false)}));
+            Assert.AreEqual(new Point(0, 1), FindTheEndOfTheSection(new Point(0, 0), new Instructions[] {
+                new Instructions(true, false, false, false),
+                new Instructions(false, false, true, false),
+                new Instructions(true, false, false, false),
+                new Instructions(false, false, false, true),
+                new Instructions(false, true, false, false),
+                new Instructions(true, false, false, false),
+                new Instructions(false, false, true, false)}));
+            Assert.AreEqual(new Point(1, 4), FindTheEndOfTheSection(new Point(2, 3), new Instructions[] { new Instructions(true, false, false, true) }));
         }
 
         public struct Instructions
@@ -20,7 +27,7 @@ namespace Sections
             public bool down;
             public bool right;
             public bool left;
-          
+
             public Instructions(bool up, bool down, bool right, bool left)
             {
                 this.up = up;
@@ -29,7 +36,7 @@ namespace Sections
                 this.left = left;
             }
         }
-          
+
         public struct Point
         {
             public int x;
@@ -42,14 +49,20 @@ namespace Sections
             }
         }
 
-        static Point FindTheEndOfTheSection(Point point, Instructions instructions)
-        {
-
-            if (instructions.up) point.y += 1;
-            if (instructions.down) point.y -= 1;
-            if (instructions.right) point.x += 1;
-            if (instructions.left) point.x -= 1;
+            static Point FindTheEndOfTheSection(Point point, Instructions[] instructions)
+            {
+            Point[] savedPoints = new Point[instructions.Length];
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                if (instructions[i].up) point.y += 1;
+                if (instructions[i].down) point.y -= 1;
+                if (instructions[i].right) point.x += 1;
+                if (instructions[i].left) point.x -= 1;
+                savedPoints[i] = new Point(point.x, point.y);
+                if (point.x == savedPoints[i].x && point.y == savedPoints[i].y)
+                    break;
+            }
             return point;
+            }
         }
     }
-}
