ff8c7eaddc262a2708d11dd4fe11b6f43bd193aa
norisradu <noris.gavrilas@yahoo.com>
2016-03-03 20:58:44 +0200

    Modified test names

--- Sections/Sections/SectionsTests.cs
+++ Sections/Sections/SectionsTests.cs
@@ -7,9 +7,14 @@ namespace Sections
     public class SectionsTests
     {
         [TestMethod]
-        public void FindTheEndOfTheSection()
-        {
+        public void ShouldReturnTheEndOfTheSection()
+        { 
             Assert.AreEqual(new Point(0, 1), FindTheEndOfTheSection(new Point(0, 0), new Instructions[] { new Instructions(true, false, false, false)}));
+            Assert.AreEqual(new Point(1, 4), FindTheEndOfTheSection(new Point(2, 3), new Instructions[] { new Instructions(true, false, false, true) }));
+        }
+        [TestMethod]
+        public void ShouldReturnTheFirstIntersection()
+        {
             Assert.AreEqual(new Point(0, 1), FindTheEndOfTheSection(new Point(0, 0), new Instructions[] {
                 new Instructions(true, false, false, false),
                 new Instructions(false, false, true, false),
@@ -18,7 +23,16 @@ namespace Sections
                 new Instructions(false, true, false, false),
                 new Instructions(true, false, false, false),
                 new Instructions(false, false, true, false)}));
-            Assert.AreEqual(new Point(1, 4), FindTheEndOfTheSection(new Point(2, 3), new Instructions[] { new Instructions(true, false, false, true) }));
+        }
+        [TestMethod]
+        public void ShouldReturnFirstPoint()
+        {
+            Assert.AreEqual(new Point(0, 0), FindTheEndOfTheSection(new Point(0, 0), new Instructions[]
+            {
+                new Instructions(false, false, true, false),
+                new Instructions(true, false, false, false),
+                new Instructions(false, false, false, true),
+                new Instructions(false, true, false, false) }));
         }
 
         public struct Instructions
@@ -54,6 +68,7 @@ namespace Sections
             Point[] savedPoints = new Point[instructions.Length];
             for (int i = 0; i < instructions.Length; i++)
             {
+                
                 if (instructions[i].up) point.y += 1;
                 if (instructions[i].down) point.y -= 1;
                 if (instructions[i].right) point.x += 1;
@@ -61,6 +76,7 @@ namespace Sections
                 savedPoints[i] = new Point(point.x, point.y);
                 if (point.x == savedPoints[i].x && point.y == savedPoints[i].y)
                     break;
+
             }
             return point;
             }
