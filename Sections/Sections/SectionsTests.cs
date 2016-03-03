7ac14c2c7411842061ed7418e5e02409fe23e30b
norisradu <noris.gavrilas@yahoo.com>
2016-03-03 18:57:43 +0200

    Modified structures and function

--- Sections/Sections/SectionsTests.cs
+++ Sections/Sections/SectionsTests.cs
@@ -7,44 +7,47 @@ namespace Sections
     public class SectionsTests
     {
         [TestMethod]
-        public void EightSections() { 
-            var firstIntersection = new Section(0, 1);
-            var sections = new Section[] {
-             new Section(0, 1),
-             new Section(1, 0),
-             new Section(1, 0),
-             new Section(0, 1),
-             new Section(-1, 0),
-             new Section(0, 1),
-             new Section(-1, 0),
-             new Section(0, -1),
-             new Section(0, -1) };
-            Assert.AreEqual(firstIntersection, GetSection(sections));
+        public void FindTheEndOfTheSection()
+        {
+            Assert.AreEqual(new Point(0, 1), FindTheEndOfTheSection(new Point(0, 0), new Instructions(true, false, false, false)));
         }
 
-        public struct Section
+        public struct Instructions
+        {
+            public bool up;
+            public bool down;
+            public bool right;
+            public bool left;
+          
+            public Instructions(bool up, bool down, bool right, bool left)
+            {
+                this.up = up;
+                this.down = down;
+                this.right = right;
+                this.left = left;
+            }
+        }
+          
+        public struct Point
         {
             public int x;
             public int y;
-          
-            public Section(int x, int y)
+
+            public Point(int x, int y)
             {
                 this.x = x;
                 this.y = y;
             }
         }
 
-        public Section GetSection(Section[] sections)
+        static Point FindTheEndOfTheSection(Point point, Instructions instructions)
         {
-            int totalX = 0;
-            int totalY = 0;
-            for (int i = 0; i < sections.Length; i++)
-            {
-                totalX += sections[i].x;
-                totalY += sections[i].y;
-            }
-            return new Section(totalX, totalY);
-        }
 
+            if (instructions.up) point.y += 1;
+            if (instructions.down) point.y -= 1;
+            if (instructions.right) point.x += 1;
+            if (instructions.left) point.x -= 1;
+            return point;
+        }
     }
 }
