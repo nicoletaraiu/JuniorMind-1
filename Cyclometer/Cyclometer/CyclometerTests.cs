d1370ccdb9580795830c3de9845ebcbde9c51614
norisradu <noris.gavrilas@yahoo.com>
2016-03-07 23:10:04 +0200

    The test for total distance fails

--- Cyclometer/Cyclometer/CyclometerTests.cs
+++ Cyclometer/Cyclometer/CyclometerTests.cs
@@ -26,6 +26,15 @@ namespace Cyclometer
             var cyclist = new Cyclist("Ion", 0.7, new Records[] { new Records(4, 1), new Records(3, 2), new Records(2, 3) });
             Assert.AreEqual(19.782, CalculateTotalDistanceOfOneCyclist(cyclist));
         }
+        [TestMethod]
+        public void ShouldReturnTotalDistance()
+        {
+            var cyclists = new Cyclist[] {
+            new Cyclist("Ion", 0.7, new Records[] { new Records(4, 1), new Records(3, 2), new Records(2, 3) }),
+            new Cyclist("Gheorghe", 0.6, new Records[] { new Records(3, 1), new Records(4, 2), new Records(5, 3) }),
+            new Cyclist("Raul", 0.5, new Records[] { new Records(2, 1), new Records(6, 2), new Records(3, 3) }) };
+            Assert.AreEqual(0, CalculateTotalDistance(cyclists));
+        }
 
         public struct Cyclist {
             public string name;
@@ -78,6 +87,16 @@ namespace Cyclometer
             for (int i = 0; i < cyclist.records.Length; i++)
             {
                 totalDistance += 3.14 * cyclist.diameter * cyclist.records[i].rotations;
+            }
+            return totalDistance;
+        }
+        
+        double CalculateTotalDistance(Cyclist[] cyclists)
+        {
+            double totalDistance = 0;
+            for (int i = 0; i < cyclists.Length; i++)
+            {
+                totalDistance += CalculateTotalDistanceOfOneCyclist(cyclists[i]);
             }
             return totalDistance;
         }
