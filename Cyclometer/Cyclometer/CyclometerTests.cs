ed7dad8d7b0c5d6f7878837ad16926b912edb80e
norisradu <noris.gavrilas@yahoo.com>
2016-03-07 23:05:44 +0200

    Calculate the distance traveled by one cyclist

--- Cyclometer/Cyclometer/CyclometerTests.cs
+++ Cyclometer/Cyclometer/CyclometerTests.cs
@@ -6,32 +6,78 @@ namespace Cyclometer
     [TestClass]
     public class CyclometerTests
     {
-        [TestMethod]
+       /* [TestMethod]
         public void ShouldReturnOneRotation()
         {
             Assert.AreEqual(5.652, CalculateTotalDistance(new Cyclist[] {
-            new Cyclist("Ion", 0.7),
-            new Cyclist("Gheorghe", 0.6),
-            new Cyclist("Raul", 0.5)}));
+            new Cyclist("Ion", 0.7, new Records[] { new Records(4, 1), new Records(3, 2), new Records(2, 3) }) ,
+            new Cyclist("Gheorghe", 0.6, new Records[] { new Records (3, 1), new Records(4, 2), new Records(5, 3) }),
+            new Cyclist("Raul", 0.5, new Records[] { new Records(2, 1), new Records(6, 2), new Records(3, 3)}) }));
+        }*/
+        [TestMethod]
+        public void ShouldReturnRotationsSum()
+        {
+            var records = new Records[] { new Records(4, 1), new Records(3, 2), new Records(2, 3) };
+            Assert.AreEqual(9, CalculateRotationsSum(records));
+        }
+        [TestMethod]
+        public void ShouldReturnTotalDistanceOfOneCyclist()
+        {
+            var cyclist = new Cyclist("Ion", 0.7, new Records[] { new Records(4, 1), new Records(3, 2), new Records(2, 3) });
+            Assert.AreEqual(19.782, CalculateTotalDistanceOfOneCyclist(cyclist));
         }
 
         public struct Cyclist {
             public string name;
             public double diameter;
+            public Records[] records;
 
-            public Cyclist(string name, double diameter)
+            public Cyclist(string name, double diameter, Records[] records)
             {
                 this.name = name;
                 this.diameter = diameter;
+                this.records = records;
             }
         }
 
-        double CalculateTotalDistance(Cyclist[] bikes)
+        public struct Records
+        {
+            public int rotations;
+            public int second;
+
+            public Records(int rotations, int second)
+            {
+                this.rotations = rotations;
+                this.second = second;
+            }
+        }
+
+        double CalculateDistanceOfOneRotation(Cyclist[] cyclists)
+        {
+            double oneRotationDistance = 0;
+            for (int i = 0; i < cyclists.Length; i++)
+            {
+                oneRotationDistance = 3.14 * cyclists[i].diameter;
+            }
+            return oneRotationDistance;
+        }
+        
+        double CalculateRotationsSum(Records[] records)
+        {
+            int sum = 0;
+            for (int i = 0; i < records.Length; i++)
+            {
+                sum += records[i].rotations;
+            }
+            return sum;
+        }
+
+        double CalculateTotalDistanceOfOneCyclist(Cyclist cyclist)
         {
             double totalDistance = 0;
-            for (int i = 0; i < bikes.Length; i++)
+            for (int i = 0; i < cyclist.records.Length; i++)
             {
-                totalDistance += 3.14 * bikes[i].diameter;
+                totalDistance += 3.14 * cyclist.diameter * cyclist.records[i].rotations;
             }
             return totalDistance;
         }
