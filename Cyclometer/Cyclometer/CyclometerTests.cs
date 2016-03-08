e2d1f170b4d7032418129b1a122c33b5f035dcba
norisradu <noris.gavrilas@yahoo.com>
2016-03-08 21:23:26 +0200

    Passed test for cyclist with best average speed

--- Cyclometer/Cyclometer/CyclometerTests.cs
+++ Cyclometer/Cyclometer/CyclometerTests.cs
@@ -57,7 +57,8 @@ namespace Cyclometer
             new Cyclist("Raul", 0.5, new Records[] { new Records(2, 1), new Records(6, 2), new Records(3, 3) }) ,
             new Cyclist("Mihai", 0.8, new Records[] { new Records(5, 1), new Records(5, 2), new Records(5, 3) }),
             new Cyclist("Darius", 0.6, new Records[] { new Records(3, 1), new Records(2, 2), new Records(1, 3)}) };
-            Assert.AreEqual(new Cyclist("Mihai", 0.8, new Records[] { new Records(5, 1), new Records(5, 2), new Records(5, 3) }), FindCyclistWithBestAverageSpeed(cyclists));
+            Assert.AreEqual(12.56, CalculateBestAverageSpeed(cyclists), 0.0001);
+            Assert.AreEqual(cyclists[3], FindCyclistWithBestAverageSpeed(cyclists));
         }
 
         public struct Cyclist {
