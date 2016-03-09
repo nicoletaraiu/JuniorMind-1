69fd7bdc8ccd7c6574b5aa7eda3e93fa14d7ca0b
norisradu <noris.gavrilas@yahoo.com>
2016-03-09 22:45:47 +0200

    The test fails

--- Fibonacci/Fibonacci/FibonacciTests.cs
+++ Fibonacci/Fibonacci/FibonacciTests.cs
@@ -4,11 +4,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Fibonacci
 {
     [TestClass]
-    public class UnitTest1
+    public class FibonacciTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturnFive()
         {
+            Assert.AreEqual(0, Fibonacci(7));
+        }
+
+
+        public int Fibonacci(int number)
+        {
+            int previous = 0;
+            return Fibonacci(number, ref previous);
+        }
+
+        public int Fibonacci(int number, ref int previous)
+        {
+            if (number < 2) return number;
+            int beforePrevious = 0;
+            previous = Fibonacci(number - 1, ref beforePrevious);
+            return previous + beforePrevious;
         }
     }
 }
+
