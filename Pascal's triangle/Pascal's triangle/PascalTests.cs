075694a84c4797ffd7c73c010bbf17d878cf56e3
norisradu <noris.gavrilas@yahoo.com>
2016-03-12 12:25:22 +0200

    Add empty Pascal project

---
+++ Pascal's triangle/Pascal's triangle/PascalTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pascal_s_triangle
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
