1437d50d9a60bcaaec6911c126f0eaf10bf352e9
norisradu <noris.gavrilas@yahoo.com>
2016-03-13 22:52:40 +0200

    First tests passed

---
+++ ReverseString/ReverseString/ReverseStringTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseString
+{
+    [TestClass]
+    public class ReverseStringTests
+    {
+        [TestMethod]
+        public void FiveLetters()
+        {
+            Assert.AreEqual("edcba", ReverseString("abcde"));
+        }
+        [TestMethod]
+        public void ThreeLetters()
+        {
+            Assert.AreEqual("cba", ReverseString("abc"));
+        }
+        [TestMethod]
+        public void TwoLetters()
+        {
+            Assert.AreEqual("ba", ReverseString("ab"));
+        }
+
+        public string ReverseString(string word)
+        {
+            string previous = string.Empty;
+            return ReverseString(word, ref previous);
+        }
+
+        public string ReverseString(string word, ref string previous)
+        {
+            string reversed = string.Empty;
+            if (word.Length < 2) return word;
+            int length = word.Length;
+            while (length > 0)
+            {
+                reversed += word[length - 1] + ReverseString(previous);
+                length--;
+            }
+            return reversed;
+        }
+
+    }
+}
