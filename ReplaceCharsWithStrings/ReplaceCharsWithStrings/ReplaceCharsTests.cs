079df29a226f4f04c5310b20c564090a03181611
norisradu <noris.gavrilas@yahoo.com>
2016-03-15 09:45:37 +0200

    First tests passed

--- ReplaceCharsWithStrings/ReplaceCharsWithStrings/ReplaceCharsTests.cs
+++ ReplaceCharsWithStrings/ReplaceCharsWithStrings/ReplaceCharsTests.cs
@@ -11,10 +11,30 @@ namespace ReplaceCharsWithStrings
         {
             Assert.AreEqual("abcghief", ReplaceChar("abcde", 'd', "ghi"));
         }
+        [TestMethod]
+        public void SecondTest()
+        {
+            Assert.AreEqual("JuniorMind", ReplaceChar("Juniormind", 'm', "M"));
+        }
+        [TestMethod]
+        public void ShouldReturnTheOtherChar()
+        {
+            Assert.AreEqual("nopq", ReplaceChar("m", 'm', "nopq"));
+        }
+        [TestMethod]
+        public void ShouldReturnTheString()
+        {
+            Assert.AreEqual("m", ReplaceChar("m", 'n', "o"));
+        }
 
         public string ReplaceChar(string s, char toReplace, string toReplaceWith)
         {
-            return s;
+            if (s.Length < 2)
+            {
+                if (s[0] == toReplace) return toReplaceWith.ToString();
+                else return s;
+            }
+            return s[0] + ReplaceChar(s.Substring(1, s.Length - 1), toReplace, toReplaceWith);
         }
 
     }
