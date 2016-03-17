d3fe0a73793f11602b1e3efc6606dffb79f0f20f
norisradu <noris.gavrilas@yahoo.com>
2016-03-17 22:06:17 +0200

    Almost working

--- Towers of Hanoi/Towers of Hanoi/TowersTests.cs
+++ Towers of Hanoi/Towers of Hanoi/TowersTests.cs
@@ -4,11 +4,34 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Towers_of_Hanoi
 {
     [TestClass]
-    public class UnitTest1
+    public class TowersTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ThreeDiscs()
         {
+            CollectionAssert.AreEqual(new int[] { 3, 2, 1 }, TOH(3, new int[] { 3, 2, 1 }, new int[3], new int[3]));
+        }
+
+        static int[] TOH(int disks, int[] A, int[] B, int[] C)
+        {
+            if (disks == 1)
+            {
+                return Move(disks, A, C);
+            }
+            else
+            {
+                TOH(disks - 1, A, B, C);
+                Move(disks, A, C);
+                TOH(disks - 1, B, A, C);
+            }
+            return C;
+        }
+
+        static int[] Move(int diskNumber, int[] from, int[] to)
+        {
+            to[diskNumber - 1] = from[diskNumber - 1];
+            Array.Resize(ref from, from.Length - 1);
+            return from;
         }
     }
 }
