8a45b0c62ae2b0f4515377ea9130dae5c791d76c
norisradu <noris.gavrilas@yahoo.com>
2016-03-17 22:22:52 +0200

    Generator function implemented

--- Towers of Hanoi/Towers of Hanoi/TowersTests.cs
+++ Towers of Hanoi/Towers of Hanoi/TowersTests.cs
@@ -13,6 +13,12 @@ namespace Towers_of_Hanoi
             CollectionAssert.AreEqual(new int[] { 4, 3, 2, 1 }, TOH(4, new int[] { 4, 3, 2, 1 }, new int[4], new int[4]));
             CollectionAssert.AreEqual(new int[] { 7, 6, 5, 4, 3, 2, 1 }, TOH(7, new int[] { 7, 6, 5, 4, 3, 2, 1, 0 }, new int[7], new int[7]));
         }
+        [TestMethod]
+        public void ShouldGenerateNumbersArray()
+        {
+            CollectionAssert.AreEqual(new int[] { 5, 4, 3, 2, 1 }, Generate(5));
+            CollectionAssert.AreEqual(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, Generate(10));
+        }
 
         static int[] TOH(int disks, int[] A, int[] B, int[] C)
         {
@@ -34,6 +40,18 @@ namespace Towers_of_Hanoi
             to[diskNumber - 1] = from[diskNumber - 1];
             Array.Resize(ref from, from.Length - 1);
             return from;
+        }
+
+        static int[] Generate(int numbers)
+        {
+            int keeper = numbers;
+            int[] generated = new int[numbers];
+            for (int i = 0; i < keeper; i++)
+            {
+                generated[i] = numbers;
+                numbers--;
+            }
+            return generated;
         }
     }
 }
