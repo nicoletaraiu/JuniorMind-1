68d234c8f5bf54727a28182701ae2a7418cdb7d1
norisradu <noris.gavrilas@yahoo.com>
2016-03-19 18:28:16 +0200

    Two tests passed

--- Calculator/Calculator/CalculatorTests.cs
+++ Calculator/Calculator/CalculatorTests.cs
@@ -9,15 +9,34 @@ namespace Calculator
         [TestMethod]
         public void ShouldReturnOne()
         {
-            Assert.AreEqual(1, Compute("1"));
+            int i = 0;
+            Assert.AreEqual(1, Compute("1", ref i));
+        }
+        [TestMethod]
+        public void ShouldReturn12()
+        {
+            int i = 0;
+            Assert.AreEqual(12, Compute("* 3 4", ref i));
         }
 
-        double Compute(string s)
+        double Compute(string s, ref int i)
         {
+            string[] elements = s.Split(' ');
+            string currentElement = elements[i++];
+
             double result = 0;
-            if (double.TryParse(s, out result))
+            if (double.TryParse(currentElement, out result))
+            {
                 return result;
-            return result;
+            }
+
+            switch (currentElement)
+            {
+                case "+": return Compute(s, ref i) + Compute(s, ref i);
+                case "-": return Compute(s, ref i) - Compute(s, ref i);
+                case "*": return Compute(s, ref i) * Compute(s, ref i);
+                default: return Compute(s, ref i) / Compute(s, ref i);
+            }
         }
     }
 }
