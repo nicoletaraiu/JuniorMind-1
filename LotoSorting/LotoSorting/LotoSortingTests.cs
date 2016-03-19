9ea9318ea64f14ca6fb540856fbf4ba37e9c1176
norisradu <noris.gavrilas@yahoo.com>
2016-03-19 20:22:03 +0200

    Implemented function for largest number

--- LotoSorting/LotoSorting/LotoSortingTests.cs
+++ LotoSorting/LotoSorting/LotoSortingTests.cs
@@ -11,10 +11,28 @@ namespace LotoSorting
         {
             CollectionAssert.AreEqual(new int[] { 49, 40, 39, 30, 19, 10 }, Sort(new int[] { 30, 40, 39, 49, 10, 19 }));
         }
-
-        int[] Sort(int[] winningnumbers)
+        [TestMethod]
+        public void ShouldReturnLargestNumber()
         {
-            return winningnumbers;
+            Assert.AreEqual(49, GetLargest(new int[] { 30, 40, 39, 49, 10, 19 }));
+            Assert.AreEqual(50, GetLargest(new int[] { 42, 50, 49, 38 }));
+        }
+
+        int[] Sort(int[] winningNumbers)
+        {
+            int[] sorted = new int[winningNumbers.Length];
+            return sorted;
+        }
+
+        int GetLargest(int[] winningNumbers)
+        {
+            int largest = winningNumbers[0];
+            for (int i = 1; i < winningNumbers.Length; i++)
+            {
+                if (winningNumbers[i] > largest)
+                    largest = winningNumbers[i];
+            }
+            return largest;
         }
     }
 }
