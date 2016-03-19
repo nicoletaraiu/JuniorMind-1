e8bb6f4214e16f771f96bb26587024f205c384b4
norisradu <noris.gavrilas@yahoo.com>
2016-03-19 20:14:39 +0200

    The test is failing now

--- LotoSorting/LotoSorting/LotoSortingTests.cs
+++ LotoSorting/LotoSorting/LotoSortingTests.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LotoSorting
 {
     [TestClass]
-    public class UnitTest1
+    public class LotoSortingTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturnSortedArray()
         {
+            CollectionAssert.AreEqual(new int[] { 49, 40, 39, 30, 19, 10 }, Sort(new int[] { 30, 40, 39, 49, 10, 19 }));
+        }
+
+        int[] Sort(int[] winningnumbers)
+        {
+            return winningnumbers;
         }
     }
 }
