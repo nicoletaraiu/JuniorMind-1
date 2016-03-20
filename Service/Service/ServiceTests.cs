a765b59f24002a9914811e9b0aece871317f2cb0
norisradu <noris.gavrilas@yahoo.com>
2016-03-20 12:59:42 +0200

    Second test passed

--- Service/Service/ServiceTests.cs
+++ Service/Service/ServiceTests.cs
@@ -7,10 +7,33 @@ namespace Service
     public class ServiceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturnSortedArray()
         {
             var products = new Product[] { new Product("Siemens", Priority.Low), new Product("Nokia", Priority.Medium), new Product("Alcatel", Priority.High) };
             var expected = new Product[] { new Product("Alcatel", Priority.High), new Product("Nokia", Priority.Medium), new Product("Siemens", Priority.Low) };
+            CollectionAssert.AreEqual(expected, Sort(products));
+        }
+        [TestMethod]
+        public void ShouldReturnSortedArraY()
+        {
+            var products = new Product[]
+            {
+                new Product("Nokia", Priority.Medium),
+                new Product("Sagem", Priority.High),
+                new Product("Siemens", Priority.Low),
+                new Product("Samsung", Priority.Medium),
+                new Product("Huawei", Priority.Low),
+                new Product("Alcatel", Priority.High)
+            };
+            var expected = new Product[]
+            {
+                new Product("Sagem", Priority.High),
+                new Product("Alcatel", Priority.High),
+                new Product("Nokia", Priority.Medium),
+                new Product("Samsung", Priority.Medium),
+                new Product("Siemens", Priority.Low),
+                new Product("Huawei", Priority.Low)
+            };
             CollectionAssert.AreEqual(expected, Sort(products));
         }
         
