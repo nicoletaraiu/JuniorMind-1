5892f212d215688220a5b2af12b5b9ca69d6d364
norisradu <noris.gavrilas@yahoo.com>
2016-03-20 12:24:05 +0200

    Implemented the struct; failed the test

--- Service/Service/ServiceTests.cs
+++ Service/Service/ServiceTests.cs
@@ -4,11 +4,31 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Service
 {
     [TestClass]
-    public class UnitTest1
+    public class ServiceTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+            var products = new Product[] { new Product("Siemens", "Low"), new Product("Nokia", "Medium"), new Product("Alcatel", "High") };
+            var expected = new Product[] { new Product("Alcatel", "High"), new Product("Nokia", "Medium"), new Product("Siemens", "Low") };
+            CollectionAssert.AreEqual(expected, Sort(products));
+        }
+
+        public struct Product
+        {
+            public string name;
+            public string priority;
+
+            public Product(string name, string priority)
+            {
+                this.name = name;
+                this.priority = priority;
+            }
+        }
+
+        static Product[] Sort(Product[] products)
+        {
+            return products;
         }
     }
 }
