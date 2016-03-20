591c97083286cdfdf3ffad044c2b61092a1a5d85
norisradu <noris.gavrilas@yahoo.com>
2016-03-20 11:26:22 +0200

    First test passed

--- LotoSorting/LotoSorting/LotoSortingTests.cs
+++ LotoSorting/LotoSorting/LotoSortingTests.cs
@@ -9,30 +9,27 @@ namespace LotoSorting
         [TestMethod]
         public void ShouldReturnSortedArray()
         {
-            CollectionAssert.AreEqual(new int[] { 49, 40, 39, 30, 19, 10 }, Sort(new int[] { 30, 40, 39, 49, 10, 19 }));
-        }
-        [TestMethod]
-        public void ShouldReturnLargestNumber()
-        {
-            Assert.AreEqual(49, GetLargest(new int[] { 30, 40, 39, 49, 10, 19 }));
-            Assert.AreEqual(50, GetLargest(new int[] { 42, 50, 49, 38 }));
+            CollectionAssert.AreEqual(new int[] { 10, 19, 30, 39, 40, 49 }, Sort(new int[] { 30, 40, 39, 49, 10, 19 }));
         }
 
         int[] Sort(int[] winningNumbers)
         {
-            int[] sorted = new int[winningNumbers.Length];
-            return sorted;
+            for (int i = 0; i < winningNumbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (winningNumbers[j - 1] > winningNumbers[j])
+                        Swap(ref winningNumbers[j - 1], ref winningNumbers[j]);
+                }
+            }
+            return winningNumbers;
         }
 
-        int GetLargest(int[] winningNumbers)
+        static void Swap(ref int a, ref int b)
         {
-            int largest = winningNumbers[0];
-            for (int i = 1; i < winningNumbers.Length; i++)
-            {
-                if (winningNumbers[i] > largest)
-                    largest = winningNumbers[i];
-            }
-            return largest;
+            int temp = a;
+            a = b;
+            b = temp;
         }
     }
 }
