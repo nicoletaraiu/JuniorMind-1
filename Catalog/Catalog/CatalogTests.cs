1db8be5dc36724c085859c115bc22c74f12a416b
norisradu <noris.gavrilas@yahoo.com>
2016-03-23 14:36:37 +0200

    The test is failing now

--- Catalog/Catalog/CatalogTests.cs
+++ Catalog/Catalog/CatalogTests.cs
@@ -4,11 +4,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Catalog
 {
     [TestClass]
-    public class UnitTest1
+    public class CatalogTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldCalculateGeneralAverageGrade()
         {
+            Assert.AreEqual(10, CalculateTheAverageGradeForOneSubject(new Student("Ion", new int[] { 9, 8, 10 })));
+        }
+        
+        public struct Student
+        {
+            public string name;
+            public int[] grades;
+
+            public Student(string name, int[] grades)
+            {
+                this.name = name;
+                this.grades = grades;
+            }
+        }
+
+        static int CalculateTheAverageGradeForOneSubject(Student student)
+        {
+            return 0;
         }
     }
 }
