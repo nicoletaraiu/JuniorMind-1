94b1a333889e00193a320a344889cdbc29ae6eda
norisradu <noris.gavrilas@yahoo.com>
2016-03-29 19:22:32 +0300

    The test is failing now

--- Elections/Elections/ElectionsTests.cs
+++ Elections/Elections/ElectionsTests.cs
@@ -4,11 +4,41 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Elections
 {
     [TestClass]
-    public class UnitTest1
+    public class ElectionsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldSortCandidates()
         {
+            var sorted = new Candidate[] {
+            new Candidate("Marcel", 1000),
+            new Candidate("Alex", 998),
+            new Candidate("Costel", 784),
+            new Candidate("Vasile", 554),
+            new Candidate("Cosmin", 449) };
+            CollectionAssert.AreEqual(sorted, Sort(new Candidate[] {
+                new Candidate("Vasile", 554),
+                new Candidate("Cosmin", 449),
+                new Candidate("Alex", 998),
+                new Candidate("Costel", 784),
+                new Candidate("Marcel", 1000) }));
         }
-    }
+
+        public struct Candidate
+        {
+            public string name;
+            public int votes;
+
+            public Candidate(string name, int votes)
+            {
+                this.name = name;
+                this.votes = votes;
+            }
+        }
+
+            public Candidate[] Sort(Candidate[] candidates)
+            {
+                return candidates;
+            }
+
+        }
 }
