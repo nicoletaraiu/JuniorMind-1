dbb37fb1e42d22c1d9646134f459edd88282271f
norisradu <noris.gavrilas@yahoo.com>
2016-04-04 11:10:07 +0300

    Alphabetically sorted. Sum of votes implemented.

--- Elections/Elections/ElectionsTests.cs
+++ Elections/Elections/ElectionsTests.cs
@@ -9,24 +9,34 @@ namespace Elections
         [TestMethod]
         public void ShouldSortCandidates()
         {
-            var firstList = new Candidate[] {
+            var lists = new List[] {
+            new List(new Candidate[] {
                 new Candidate("Vasile", 1200),
                 new Candidate("Cosmin", 900),
                 new Candidate("Alex", 800),
                 new Candidate("Costel", 600),
-                new Candidate("Marcel", 500) };
-            var secondList = new Candidate[] {
+                new Candidate("Marcel", 500)}),
+            new List(new Candidate[] {
                 new Candidate("Costel", 700),
                 new Candidate("Alex", 500),
                 new Candidate("Cosmin", 300),
                 new Candidate("Vasile", 200),
-                new Candidate("Marcel", 100) };
-            var thirdList = new Candidate[] {
-                new Candidate("Costel", 1200),
-                new Candidate("Mircea", 1100),
+                new Candidate("Marcel", 100) }),
+            new List(new Candidate[] {
+                new Candidate("Vasile", 1200),
+                new Candidate("Costel", 1100),
                 new Candidate("Alex", 900),
                 new Candidate("Cosmin", 700),
-                new Candidate("Marcel", 500) };
+                new Candidate("Marcel", 500) }) };
+            var sorted = new Candidate[]
+            {
+                new Candidate("Alex", 2200),
+                new Candidate("Cosmin", 1900),
+                new Candidate("Costel", 2400),
+                new Candidate("Marcel", 1100),
+                new Candidate("Vasile", 2600)   
+            };
+            CollectionAssert.AreEqual(sorted, Sort(lists));
         }
         [TestMethod]
         public void ShouldSortAlphabetically()
@@ -41,7 +51,7 @@ namespace Elections
                 new Candidate("Alex", 800),
                 new Candidate("Cosmin", 900),
                 new Candidate("Costel", 600),
-                 new Candidate("Marcel", 500),
+                new Candidate("Marcel", 500),
                 new Candidate("Vasile", 1200) };
             var secondList = new Candidate[] {
                 new Candidate("Costel", 700),
@@ -59,14 +69,12 @@ namespace Elections
             CollectionAssert.AreEqual(secondSorted, SortAlphabetically(secondList));
         }
         
-        public struct PollingStation
+        public struct List
         {
-            public string name;
             public Candidate[] candidates;
 
-            public PollingStation(string name, Candidate[] candidates)
+            public List(Candidate[] candidates)
             {
-                this.name = name;
                 this.candidates = candidates;
             }
         }
@@ -81,6 +89,29 @@ namespace Elections
                 this.name = name;
                 this.votes = votes;
             }
+        }
+
+        public static Candidate[] Sort(List[] lists)
+        {  
+            List[] sortedAlphabetically = new List[lists.Length];
+            Candidate[] sorted = new Candidate[lists[0].candidates.Length];
+            for (int i = 0; i < sortedAlphabetically.Length; i++)
+            {
+                sortedAlphabetically[i].candidates = SortAlphabetically(lists[i].candidates);
+            }
+           
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                int totalVotes = 0;
+                string name = "";
+                for (int j = 0; j < sortedAlphabetically.Length; j++)
+                {   
+                    totalVotes += sortedAlphabetically[j].candidates[i].votes;
+                    name = sortedAlphabetically[j].candidates[i].name;
+                }
+                sorted[i] = new Candidate(name, totalVotes);
+            }
+            return sorted;
         }
 
         public static Candidate[] SortAlphabetically(Candidate[] candidates)
