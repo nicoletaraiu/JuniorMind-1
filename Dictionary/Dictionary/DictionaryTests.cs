10f8266c853376b2c5acb99981bdf2a74b7b508b
norisradu <noris.gavrilas@yahoo.com>
2016-04-18 20:40:28 +0300

    Add new Dictionary project

---
+++ Dictionary/Dictionary/DictionaryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dictionary
+{
+    [TestClass]
+    public class DictionaryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
